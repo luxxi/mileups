d7470f0e0f7618c9d8499994085c7cc06efffdb4
Luka <luka@domitrovic.si>
2014-11-21 17:21:35 +0100

    image upload edit

--- app/assets/javascripts/application.js.coffee
+++ app/assets/javascripts/application.js.coffee
@@ -1,7 +1,7 @@
-
 #= require jquery
 #= require jquery_ujs
 #= require jquery-fileupload/basic
+#= require jquery-fileupload/vendor/tmpl
 #= require twitter/bootstrap
 #= require turbolinks
 #= require_tree .
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,17 +1,16 @@
 class TripsController < ApplicationController
+  before_action :authenticate_user!
 
   def dashboard
-    #@trips ||= current_user.trips
-    @trips = Trip.all
+    @trips = current_user.trips
   end
 
   def new
-    @trip = Trip.new
-    @trip.photos.build
+    @trip = current_user.trips.build
   end
 
   def create
-    @trip = current_user.trips.build(trip_params)
+    @trip = current_user.trips.create(trip_params)
     if @trip.save
       redirect_to edit_trip_path(@trip), :notice => 'Ok!'
     else
@@ -21,11 +20,21 @@ class TripsController < ApplicationController
 
   def edit
     @trip = Trip.find(params[:id])
+    @photos = @trip.photos
+  end
+
+  def upload_photos
+    @trip = Trip.find(params[:trip_id])
+    @photo = @trip.photos.create(photo_params)
   end
 
   private
   def trip_params
     params.require(:trip).permit(:title, :user_id, :photos_attributes => [:trip_id, :image])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:image, :trip_id)
   end
 
 end
