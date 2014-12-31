ef5318e291c94947075c0821a48d96adaf6f2281
mz3944 <zrimsekmatej@gmail.com>
2014-12-31 01:43:56 +0100

    Persisting name fields.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -46,6 +46,14 @@ class RegistrationsController < Devise::RegistrationsController
   def user_params
     params.require(:user).permit(:email, :password)
   end
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
 end
 
 
